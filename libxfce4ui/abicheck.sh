b232a4043b42926e27eb13833e858dfed2bbebe7
Nick Schermer <nick@xfce.org>
2011-03-27 14:00:51 +0200

    Use new portable abicheck.sh from xfconf.

--- libxfce4ui/abicheck.sh
+++ libxfce4ui/abicheck.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 #
 # Copyright (c) 2004 The GLib Development Team.
-# Copyright (c) 2007 The Xfce Development Team.
+# Copyright (c) 2005 Benedikt Meurer <benny@xfce.org>.
+# Copyright (c) 2011 Guido Berhoerster <gber@opensuse.org>
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License ONLY.
 #
-# This library is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
 
-cpp -P -DINCLUDE_INTERNAL_SYMBOLS -DINCLUDE_VARIABLES -DALL_FILES ${srcdir:-.}/libxfce4ui.symbols | sed -e '/^$/d' -e 's/ LIBXFCE4UI_GNUC.*$//' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE//' | sort > expected-abi
-nm -D .libs/libxfce4ui-1.so | grep " T\|R\|G " | cut -d ' ' -f 3 | grep -v '^_.*' | grep -v '^ *$' | sort > actual-abi
-diff -u expected-abi actual-abi && rm expected-abi actual-abi
+trap 'rm expected-abi actual-abi' EXIT
+${CPP:-cpp} -DINCLUDE_INTERNAL_SYMBOLS -DINCLUDE_VARIABLES -DALL_FILES ${srcdir:-.}/libxfce4ui.symbols | sed 's/ G_GNUC.*$//;s/ PRIVATE//;/^ *$/d;/^#/d' | sort >expected-abi
+${NM:-nm} -D -g -P .libs/libxfce4ui-1.so | awk '$2~/^[DRTG]$/&&$1~/^[^_]/{print $1}' | sort >actual-abi
+diff -u expected-abi actual-abi
