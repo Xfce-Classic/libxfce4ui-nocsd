5680b420ecd4483549a7c76bf11287769fd6cf8a
Transifex <noreplay@xfce.org>
2011-10-16 00:00:11 +0200

    Update translators, week 41.

--- xfce4-about/translators.h
+++ xfce4-about/translators.h
@@ -24,7 +24,7 @@
 #include <gtk/gtk.h>
 #include <libxfce4util/libxfce4util.h>
 
-#define TRANSLATORS_H_STAMP "2011-10-09 00:00:03"
+#define TRANSLATORS_H_STAMP "2011-10-16 00:00:03"
 #define ATSIGN "@"
 #define DOTSIGN "."
 
