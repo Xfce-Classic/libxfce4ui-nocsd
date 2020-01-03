fd0c29100fc10f127ff06662e94b6c8dc5bf8fcc
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-01-04 00:38:41 +0100

    tests: Re-order buttons

--- tests/test-ui.c
+++ tests/test-ui.c
@@ -176,9 +176,9 @@ show_xfce_titled_dialog_new_with_mixed_buttons (GtkButton *button,
   dialog_gtk3 = xfce_titled_dialog_new_with_mixed_buttons ("Settings Editor (Gtk3)", NULL,
                                                            GTK_DIALOG_DESTROY_WITH_PARENT,
                                                            "help-browser", "Help", GTK_RESPONSE_HELP,
-                                                           "window-close-symbolic", "Close", GTK_RESPONSE_OK,
                                                            "weather-snow", "Snow!", GTK_RESPONSE_APPLY,
                                                            "", "no icon", GTK_RESPONSE_CANCEL,
+                                                           "window-close-symbolic", "Close", GTK_RESPONSE_OK,
                                                            NULL);
 
   xfce_titled_dialog_set_subtitle (XFCE_TITLED_DIALOG (dialog_gtk3),
