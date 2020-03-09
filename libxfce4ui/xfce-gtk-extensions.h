3d6dd6e10866c9fc70dbd123a70746bf7926748e
Alexander Schwinn <alexxcons@xfce.org>
2020-03-09 23:23:07 +0100

    Added methods to replace deprecated GtkAction, GtkActionEntry and GtkUiManager in order to keep simple menu creation

--- libxfce4ui/xfce-gtk-extensions.h
+++ libxfce4ui/xfce-gtk-extensions.h
@@ -28,31 +28,124 @@
 
 G_BEGIN_DECLS
 
-GtkWidget *xfce_gtk_button_new_mixed               (const gchar        *stock_id,
-                                                    const gchar        *label) G_GNUC_MALLOC;
+/*
+ * List of item types which are supported by the XfceGtkActionEntry
+ * To be extended if required
+*/
+typedef enum
+{
+  XFCE_GTK_MENU_ITEM,
+  XFCE_GTK_IMAGE_MENU_ITEM,
+  XFCE_GTK_CHECK_MENU_ITEM,
+  XFCE_GTK_RADIO_MENU_ITEM,
+} XfceGtkMenuItem;
 
-GtkWidget *xfce_gtk_frame_box_new                  (const gchar        *label,
-                                                    GtkWidget         **container_return) G_GNUC_MALLOC;
+/*
+ * Replacement for the deprecated GtkActionEntry
+ * The idea is to provide a fixed list of XfceGtkActionEntrys
+ * - use 'xfce_gtk_translate_action_entries' once to translate the list
+ * - use 'xfce_gtk_accel_group_append' once to register the provided accelerators
+ * - use 'xfce_gtk_get_action_entry_by_id' to find a single entry, e.g. by using a enumeration
+ * - use 'xfce_gtk_***_new_from_action_entry' to create the specific menu- or tool-items from the entry
+*/
+typedef struct
+{
+  guint            id;                     /* unique identifier for instances of this structure (you might want to use a enum) */
+  const gchar     *accel_path;             /* The unique path, used to identify the accelerator */
+  const gchar     *default_accelerator;    /* The default key and modifier to trigger the callback linked to the entry */
 
-GtkWidget *xfce_gtk_frame_box_new_with_content     (const gchar         *label,
-                                                    GtkWidget           *content) G_GNUC_MALLOC;
+  /* menu_item data is optional, only relevant if there exists a menu_item for that accelerator */
+  XfceGtkMenuItem  menu_item_type;         /* The type of the item which should be used for creation */
+  gchar           *menu_item_label_text;   /* text which should be shown in the menu */
+  gchar           *menu_item_tooltip_text; /* optional tooltip of the item */
+  const gchar     *menu_item_icon_name;    /* optional icon name which will be used to find a image for the item */
 
-void       xfce_gtk_window_center_on_active_screen (GtkWindow           *window);
+  GCallback        callback;               /* The callback which will be triggered on activation */
 
-gboolean   xfce_gtk_menu_popup_until_mapped        (GtkMenu             *menu,
-                                                    GtkWidget           *parent_menu_shell,
-                                                    GtkWidget           *parent_menu_item,
-                                                    GtkMenuPositionFunc  func,
-                                                    gpointer             data,
-                                                    guint                button,
-                                                    guint32              activate_time);
+} XfceGtkActionEntry;
 
-gboolean   xfce_widget_reparent                    (GtkWidget           *widget,
-                                                    GtkWidget           *new_parent);
 
-gchar     *xfce_icon_name_from_desktop_id          (const gchar         *desktop_id);
 
-GIcon     *xfce_gicon_from_name                    (const gchar         *name);
+GtkWidget                *xfce_gtk_menu_item_new                          (const gchar              *label_text,
+                                                                           const gchar              *tooltip_text,
+                                                                           const gchar              *accel_path,
+                                                                           GCallback                 callback,
+                                                                           GObject                  *callback_param,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_image_menu_item_new                    (const gchar              *label_text,
+                                                                           const gchar              *tooltip_text,
+                                                                           const gchar              *accel_path,
+                                                                           GCallback                 callback,
+                                                                           GObject                  *callback_param,
+                                                                           GtkWidget                *image,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_image_menu_item_new_from_icon_name     (const gchar              *label_text,
+                                                                           const gchar              *tooltip_text,
+                                                                           const gchar              *accel_path,
+                                                                           GCallback                 callback,
+                                                                           GObject                  *callback_param,
+                                                                           const gchar              *icon_name,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_check_menu_item_new                    (const gchar              *label_text,
+                                                                           const gchar              *tooltip_text,
+                                                                           const gchar              *accel_path,
+                                                                           GCallback                 callback,
+                                                                           GObject                  *callback_param,
+                                                                           gboolean                  active,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_radio_menu_item_new                    (const gchar              *label_text,
+                                                                           const gchar              *tooltip_text,
+                                                                           const gchar              *accel_path,
+                                                                           GCallback                 callback,
+                                                                           GObject                  *callback_param,
+                                                                           gboolean                  active,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_menu_item_new_from_action_entry        (const XfceGtkActionEntry *action_entry,
+                                                                           GObject                  *callback_param,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_toggle_menu_item_new_from_action_entry (const XfceGtkActionEntry *action_entry,
+                                                                           GObject                  *callback_param,
+                                                                           gboolean                  active,
+                                                                           GtkMenuShell             *menu_to_append_item);
+GtkWidget                *xfce_gtk_tool_button_new_from_action_entry      (const XfceGtkActionEntry *action_entry,
+                                                                           GObject                  *callback_param,
+                                                                           GtkToolbar               *toolbar_to_append_item);
+GtkAccelGroup            *xfce_gtk_accel_group_append                     (GtkAccelGroup            *accel_group,
+                                                                           const XfceGtkActionEntry *action_entries,
+                                                                           guint                     n_action_entries,
+                                                                           gpointer                  callback_data);
+const XfceGtkActionEntry *xfce_gtk_get_action_entry_by_id                 (const XfceGtkActionEntry *action_entries,
+                                                                           guint                     n_action_entries,
+                                                                           guint                     id);
+void                      xfce_gtk_translate_action_entries               (XfceGtkActionEntry       *action_entries,
+                                                                           guint                     n_action_entries);
+void                      xfce_gtk_menu_append_seperator                  (GtkMenuShell             *menu);
+
+GtkWidget                *xfce_gtk_button_new_mixed                       (const gchar              *stock_id,
+                                                                           const gchar              *label) G_GNUC_MALLOC;
+
+GtkWidget                *xfce_gtk_frame_box_new                          (const gchar              *label,
+                                                                           GtkWidget               **container_return) G_GNUC_MALLOC;
+
+GtkWidget                *xfce_gtk_frame_box_new_with_content             (const gchar               *label,
+                                                                           GtkWidget                 *content) G_GNUC_MALLOC;
+
+void                      xfce_gtk_window_center_on_active_screen         (GtkWindow                 *window);
+
+gboolean                  xfce_gtk_menu_popup_until_mapped                (GtkMenu                   *menu,
+                                                                           GtkWidget                 *parent_menu_shell,
+                                                                           GtkWidget                 *parent_menu_item,
+                                                                           GtkMenuPositionFunc        func,
+                                                                           gpointer                   data,
+                                                                           guint                      button,
+                                                                           guint32                    activate_time);
+
+gboolean                  xfce_widget_reparent                            (GtkWidget                 *widget,
+                                                                           GtkWidget                 *new_parent);
+
+const gchar              *xfce_icon_name_from_desktop_id                  (const gchar               *desktop_id);
+
+GIcon                    *xfce_gicon_from_name                            (const gchar               *name);
 
 G_END_DECLS
 
